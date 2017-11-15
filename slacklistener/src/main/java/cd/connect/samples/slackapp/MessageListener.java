2aae4d3ddcd6f0bb7e88c13184fb4719e55344f0
Haichang Lai <haichang.lai@clearpoint.co.nz>
2017-11-16 08:07:32 +1300

    Changed slack listener to process specific channel messages

--- slacklistener/src/main/java/cd/connect/samples/slackapp/MessageListener.java
+++ slacklistener/src/main/java/cd/connect/samples/slackapp/MessageListener.java
@@ -1,9 +1,9 @@
 package cd.connect.samples.slackapp;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
@@ -11,26 +11,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.JMSException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MessageListener implements SlackMessagePostedListener {
 
-    Logger log = LoggerFactory.getLogger(MessageListener.class);
+	Logger log = LoggerFactory.getLogger(MessageListener.class);
 
-    Publisher publisher = new Publisher();
-    ObjectMapper mapper = new ObjectMapper();
+	Publisher publisher = new Publisher();
+	ObjectMapper mapper = new ObjectMapper();
+	List<String> slackChannels;
 
-    MessageListener() throws JMSException {
-        publisher.create("slackListener", "slack-messages");
-    }
+	MessageListener() throws JMSException {
+		publisher.create("slackListener", "slack-messages");
+		init();
+	}
 
-    public void onEvent(SlackMessagePosted slackMessagePosted, SlackSession slackSession) {
-        try {
-            ObjectNode jsonNodes = mapper.valueToTree(slackMessagePosted);
-            jsonNodes.replace("channel", mapper.valueToTree(slackMessagePosted.getChannel()));
-            jsonNodes.replace("sender", mapper.valueToTree(slackMessagePosted.getSender()));
-            publisher.sendMessgae(mapper.writeValueAsString(jsonNodes));
-        } catch (JsonProcessingException | JMSException e) {
-            log.info("unable to parse message:"+slackMessagePosted.getMessageContent());
-        }
-    }
+	public void init() {
+
+		String slackChannelsProperties = System.getProperty("slack.channels", "connect-testing");
+		log.info("Got properties slack.channels={}", slackChannelsProperties);
+
+		slackChannels = Arrays.asList(slackChannelsProperties.split(","));
+
+
+	}
+
+	public void onEvent(SlackMessagePosted slackMessagePosted, SlackSession slackSession) {
+		try {
+
+			SlackChannel channel = slackMessagePosted.getChannel();
+
+			if (slackChannels.contains(channel.getName())) {
+
+
+				ObjectNode jsonNodes = mapper.valueToTree(slackMessagePosted);
+				jsonNodes.replace("channel", mapper.valueToTree(channel));
+				jsonNodes.replace("sender", mapper.valueToTree(slackMessagePosted.getSender()));
+				publisher.sendMessgae(mapper.writeValueAsString(jsonNodes));
+
+				log.info("Sent message for channel: {} message: {}", channel.getName(), slackMessagePosted.getMessageContent());
+
+			} else {
+
+				log.info("Ignored message for channel: {} message: {}", channel.getName(), slackMessagePosted.getMessageContent());
+
+			}
+		} catch (JsonProcessingException | JMSException e) {
+			log.info("unable to parse message:" + slackMessagePosted.getMessageContent());
+		}
+	}
 }
